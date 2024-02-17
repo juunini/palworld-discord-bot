6293b9e82c459e62688a7f0e60170ff9c1bc23f5
juunini <juuni.ni.i@gmail.com>
2024-02-18 05:18:02 +0900

    fix palworld rcon port config type is uint64

--- src/config/init.go
+++ src/config/init.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/joho/godotenv"
@@ -26,7 +27,7 @@ func askConfigToUser() {
 	scanEnv("Discord Admin Usernames (comma separated)", &DISCORD_ADMIN_USERNAMES_STRING)
 	scanEnvWithDefaultValue("Discord Command Prefix", &DISCORD_COMMAND_PREFIX, DISCORD_COMMAND_PREFIX)
 	scanEnvWithDefaultValue("Palworld RCON Host", &PALWORLD_RCON_HOST, PALWORLD_RCON_HOST)
-	scanEnvWithDefaultValue("Palworld RCON Port", &PALWORLD_RCON_PORT, PALWORLD_RCON_PORT)
+	scanEnvWithDefaultValue("Palworld RCON Port", &PALWORLD_RCON_PORT_STRING, PALWORLD_RCON_PORT_STRING)
 	scanEnv("Palworld RCON Password", &PALWORLD_RCON_PASSWORD)
 	scanEnvWithDefaultValue("Language", &LANGUAGE, LANGUAGE)
 
@@ -60,7 +61,8 @@ func loadEnv() {
 	DISCORD_ADMIN_USERNAMES = trimSpaceAdminUsers()
 	DISCORD_COMMAND_PREFIX = os.Getenv("DISCORD_COMMAND_PREFIX")
 	PALWORLD_RCON_HOST = os.Getenv("PALWORLD_RCON_HOST")
-	PALWORLD_RCON_PORT = os.Getenv("PALWORLD_RCON_PORT")
+	PALWORLD_RCON_PORT_STRING = os.Getenv("PALWORLD_RCON_PORT")
+	PALWORLD_RCON_PORT, _ = strconv.ParseUint(PALWORLD_RCON_PORT_STRING, 10, 64)
 	PALWORLD_RCON_PASSWORD = os.Getenv("PALWORLD_RCON_PASSWORD")
 	DISCORD_DASHBOARD_CHANNEL_ID = os.Getenv("DISCORD_DASHBOARD_CHANNEL_ID")
 	DISCORD_LOG_CHANNEL_ID = os.Getenv("DISCORD_LOG_CHANNEL_ID")
