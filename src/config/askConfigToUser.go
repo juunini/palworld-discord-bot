750150b47159e86197fd1ad36b70d04ab7b70ce3
juunini <juuni.ni.i@gmail.com>
2024-02-18 02:59:12 +0900

    refactoring scan env function

--- src/config/askConfigToUser.go
+++ src/config/askConfigToUser.go
@@ -10,12 +10,12 @@ import (
 func askConfigToUser() {
 	fmt.Printf("%s.env file not found. Please provide the following information:%s\n\n", console_decoration.RED, console_decoration.RESET)
 
-	scanDiscordBotToken()
-	scanDiscordAdminUsernames()
-	scanDiscordCommandPrefix()
-	scanPalworldRconHost()
-	scanPalworldRconPort()
-	scanPalworldRconPassword()
+	scanEnv("Discord Bot Token", &DISCORD_BOT_TOKEN)
+	scanEnv("Discord Admin Usernames (comma separated)", &DISCORD_ADMIN_USERNAMES_STRING)
+	scanEnvWithDefaultValue("Discord Command Prefix", &DISCORD_COMMAND_PREFIX, DISCORD_COMMAND_PREFIX)
+	scanEnvWithDefaultValue("Palworld RCON Host", &PALWORLD_RCON_HOST, PALWORLD_RCON_HOST)
+	scanEnvWithDefaultValue("Palworld RCON Port", &PALWORLD_RCON_PORT, PALWORLD_RCON_PORT)
+	scanEnv("Palworld RCON Password", &PALWORLD_RCON_PASSWORD)
 
 	env := envString()
 	fmt.Printf(
--- src/config/scan.go
+++ src/config/scan.go
@@ -6,46 +6,18 @@ import (
 	"github.com/juunini/palworld-discord-bot/src/console_decoration"
 )
 
-func scanDiscordBotToken() {
-	fmt.Printf("Discord Bot Token: %s", console_decoration.BOLD)
-	fmt.Scanf("%s", &DISCORD_BOT_TOKEN)
+func scanEnv(message string, variable *string) {
+	fmt.Printf("- %s: %s", message, console_decoration.BOLD)
+	fmt.Scanf("%s", variable)
 	fmt.Print(console_decoration.RESET)
 }
 
-func scanDiscordAdminUsernames() {
-	fmt.Printf("Discord Admin Usernames (comma separated): %s", console_decoration.BOLD)
-	fmt.Scanf("%s", &DISCORD_ADMIN_USERNAMES_STRING)
-	fmt.Print(console_decoration.RESET)
-}
-
-func scanDiscordCommandPrefix() {
-	fmt.Printf("Discord Command Prefix(default: %s): %s", DISCORD_COMMAND_PREFIX, console_decoration.BOLD)
-
-	var prefix string
-	fmt.Scanf("%s", &prefix)
+func scanEnvWithDefaultValue(message string, variable *string, defaultValue string) {
+	fmt.Printf("- %s (default: %s): %s", message, defaultValue, console_decoration.BOLD)
+	fmt.Scanf("%s", variable)
 	fmt.Print(console_decoration.RESET)
 
-	if prefix == "" {
-		return
+	if *variable == "" {
+		*variable = defaultValue
 	}
-
-	DISCORD_COMMAND_PREFIX = prefix
-}
-
-func scanPalworldRconHost() {
-	fmt.Printf("Palworld RCON Host: %s", console_decoration.BOLD)
-	fmt.Scanf("%s", &PALWORLD_RCON_HOST)
-	fmt.Print(console_decoration.RESET)
-}
-
-func scanPalworldRconPort() {
-	fmt.Printf("Palworld RCON Port: %s", console_decoration.BOLD)
-	fmt.Scanf("%s", &PALWORLD_RCON_PORT)
-	fmt.Print(console_decoration.RESET)
-}
-
-func scanPalworldRconPassword() {
-	fmt.Printf("Palworld RCON Password: %s", console_decoration.BOLD)
-	fmt.Scanf("%s", &PALWORLD_RCON_PASSWORD)
-	fmt.Print(console_decoration.RESET)
 }
