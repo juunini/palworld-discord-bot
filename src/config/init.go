de878a5bd62ec40916402630eb24f316df770101
juunini <juuni.ni.i@gmail.com>
2024-02-18 02:58:52 +0900

    add loadEnv function in config

--- src/config/init.go
+++ src/config/init.go
@@ -14,13 +14,7 @@ func Init() {
 		return
 	}
 
-	DISCORD_BOT_TOKEN = os.Getenv("DISCORD_BOT_TOKEN")
-	DISCORD_ADMIN_USERNAMES_STRING = os.Getenv("DISCORD_ADMIN_USERS")
-	DISCORD_ADMIN_USERNAMES = trimSpaceAdminUsers()
-	DISCORD_COMMAND_PREFIX = os.Getenv("DISCORD_COMMAND_PREFIX")
-	PALWORLD_RCON_HOST = os.Getenv("PALWORLD_RCON_HOST")
-	PALWORLD_RCON_PORT = os.Getenv("PALWORLD_RCON_PORT")
-	PALWORLD_RCON_PASSWORD = os.Getenv("PALWORLD_RCON_PASSWORD")
+	loadEnv()
 }
 
 func trimSpaceAdminUsers() (users []string) {
@@ -30,4 +24,14 @@ func trimSpaceAdminUsers() (users []string) {
 		users = append(users, strings.TrimSpace(user))
 	}
 	return
+}
+
+func loadEnv() {
+	DISCORD_BOT_TOKEN = os.Getenv("DISCORD_BOT_TOKEN")
+	DISCORD_ADMIN_USERNAMES_STRING = os.Getenv("DISCORD_ADMIN_USERS")
+	DISCORD_ADMIN_USERNAMES = trimSpaceAdminUsers()
+	DISCORD_COMMAND_PREFIX = os.Getenv("DISCORD_COMMAND_PREFIX")
+	PALWORLD_RCON_HOST = os.Getenv("PALWORLD_RCON_HOST")
+	PALWORLD_RCON_PORT = os.Getenv("PALWORLD_RCON_PORT")
+	PALWORLD_RCON_PASSWORD = os.Getenv("PALWORLD_RCON_PASSWORD")
 }
