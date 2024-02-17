3398bbf5963539b990d4b88f5e00e563daca8347
juunini <juuni.ni.i@gmail.com>
2024-02-18 05:43:23 +0900

    refactoring shutdown command

--- src/bot/commands/shutdown.go
+++ src/bot/commands/shutdown.go
@@ -16,21 +16,11 @@ func shutdown(command string) string {
 
 	defer client.Disconnect()
 
-	paramString, _ := strings.CutPrefix(command, "shutdown ")
-	params := strings.Split(paramString, " ")
-
-	if len(params) < 2 {
-		console_decoration.PrintError(i18n.WrongParameters)
-		return i18n.WrongParameters
+	seconds, message, errorMessage := shutdownParameters(command)
+	if errorMessage != "" {
+		return errorMessage
 	}
 
-	seconds, err := utils.ToUint(params[0])
-	if err != nil {
-		console_decoration.PrintError(i18n.WrongParameters + ": " + err.Error())
-		return i18n.WrongParameters
-	}
-
-	message := params[1]
 	response, err := client.Shutdown(seconds, message)
 	if err != nil {
 		console_decoration.PrintError(i18n.FailedToShutdownCommand + ": " + err.Error())
@@ -38,4 +28,23 @@ func shutdown(command string) string {
 	}
 
 	return response
+}
+
+func shutdownParameters(command string) (uint, string, string) {
+	paramString, _ := strings.CutPrefix(command, "shutdown ")
+	params := strings.Split(paramString, " ")
+
+	if len(params) < 2 {
+		console_decoration.PrintError(i18n.WrongParameters)
+		return 0, "", i18n.WrongParameters
+	}
+
+	seconds, err := utils.ToUint(params[0])
+	if err != nil {
+		console_decoration.PrintError(i18n.WrongParameters + ": " + err.Error())
+		return 0, "", i18n.WrongParameters
+	}
+
+	message := params[1]
+	return seconds, message, ""
 }
