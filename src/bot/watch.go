f50cc9c2f4a26954c9828e844592f426ed7f6b3a
i쥬니니 <juuni.ni.i@gmail.com>
2024-02-18 15:03:28 +0900

    add online player dashboard function

--- src/bot/watch.go
+++ src/bot/watch.go
@@ -15,6 +15,7 @@ func Watch(session *discordgo.Session) {
 
 	sendNewPlayers(session)
 	sendLeftPlayers(session)
+	sendOnlinePlayerDashboard(session)
 }
 
 func sendNewPlayers(session *discordgo.Session) {
@@ -28,4 +29,44 @@ func sendLeftPlayers(session *discordgo.Session) {
 	for _, player := range watchers.LeftPlayers {
 		session.ChannelMessageSend(config.DISCORD_LOG_CHANNEL_ID, player.Username+" left the server")
 	}
+}
+
+func sendOnlinePlayerDashboard(session *discordgo.Session) {
+	if len(watchers.NewPlayers) == 0 && len(watchers.LeftPlayers) == 0 {
+		return
+	}
+
+	content := ""
+
+	for _, player := range watchers.OnlinePlayers {
+		content += fmt.Sprintf("- %s\n", player.Username)
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title: fmt.Sprintf("Online Players (%d)", len(watchers.OnlinePlayers)),
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Value:  content,
+				Inline: false,
+			},
+		},
+	}
+
+	if config.DISCORD_DASHBAORD_ONLINE_PLAYERS_MESSAGE_ID == "" {
+		message, err := session.ChannelMessageSendEmbed(config.DISCORD_DASHBOARD_CHANNEL_ID, embed)
+		if err != nil {
+			return
+		}
+
+		config.DISCORD_DASHBAORD_ONLINE_PLAYERS_MESSAGE_ID = message.ID
+		config.SaveEnv()
+		return
+	}
+
+	_, err := session.ChannelMessageEditEmbed(config.DISCORD_DASHBOARD_CHANNEL_ID, config.DISCORD_DASHBAORD_ONLINE_PLAYERS_MESSAGE_ID, embed)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
