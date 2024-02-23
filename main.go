008d000172c782024e8fa5d72b7503dd20dad768
juunini <juuni.ni.i@gmail.com>
2024-02-24 03:27:00 +0900

    move signal check to main function

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/juunini/palworld-discord-bot/src/bot"
 	"github.com/juunini/palworld-discord-bot/src/config"
+	"github.com/juunini/palworld-discord-bot/src/console_decoration"
 	"github.com/juunini/palworld-discord-bot/src/i18n"
 	"github.com/juunini/palworld-discord-bot/src/web"
 )
@@ -19,16 +23,25 @@ func main() {
 		go web.Listen(config.WEB_SERVER_PORT)
 	}
 
-	session := bot.New(config.DISCORD_BOT_TOKEN)
+	if config.DISCORD_BOT_ENABLED {
+		session := bot.New(config.DISCORD_BOT_TOKEN)
 
-	session.AddHandler(bot.Handler)
+		session.AddHandler(bot.Handler)
 
-	go func() {
-		for {
-			bot.Watch(session)
-			time.Sleep(10 * time.Second)
-		}
-	}()
+		go func() {
+			for {
+				bot.Watch(session)
+				time.Sleep(10 * time.Second)
+			}
+		}()
 
-	bot.Connect(session)
+		bot.Connect(session)
+	}
+
+	console_decoration.PrintSuccess(i18n.BotRunningStart)
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	web.Shutdown()
 }
--- src/bot/connect.go
+++ src/bot/connect.go
@@ -1,14 +1,8 @@
 package bot
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/bwmarrin/discordgo"
 	"github.com/juunini/palworld-discord-bot/src/console_decoration"
-	"github.com/juunini/palworld-discord-bot/src/i18n"
-	"github.com/juunini/palworld-discord-bot/src/web"
 )
 
 func Connect(session *discordgo.Session) {
@@ -16,13 +10,4 @@ func Connect(session *discordgo.Session) {
 		console_decoration.PrintError("error opening connection")
 		panic(err)
 	}
-
-	console_decoration.PrintSuccess(i18n.BotRunningStart)
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	session.Close()
-	// FIXME: move upper code and under line code to main.go
-	web.Shutdown()
 }
