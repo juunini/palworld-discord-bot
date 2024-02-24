f0dcc7a53fb17392756f385e69c8e32ee270d109
juunini <juuni.ni.i@gmail.com>
2024-02-25 01:04:28 +0900

    web: refactoring variables

---
+++ src/web/public/components/elements.js
@@ -0,0 +1,33 @@
+export const elements = {
+  webServerEnabled: document.getElementById("webServerEnabled"),
+  webServerPort: document.getElementById("webServerPort"),
+
+  language: document.getElementById("language"),
+
+  discordBotEnabled: document.getElementById("discordBotEnabled"),
+  discordBotToken: document.getElementById("discordBotToken"),
+  discordAdminUsernames: document.getElementById("discordAdminUsernames"),
+  discordCommandCaseSensitive: document.getElementById("discordCommandCaseSensitive"),
+  discordCommandPrefix: document.getElementById("discordCommandPrefix"),
+
+  palworldRconEnabled: document.getElementById("palworldRconEnabled"),
+  palworldRconHost: document.getElementById("palworldRconHost"),
+  palworldRconPort: document.getElementById("palworldRconPort"),
+  palworldAdminPassword: document.getElementById("palworldAdminPassword"),
+  palworldServerFilePath: document.getElementById("palworldServerFilePath"),
+  palworldServerExecuteFlags: document.getElementById("palworldServerExecuteFlags"),
+
+  discordDashboardChannelId: document.getElementById("discordDashboardChannelId"),
+  discordLogChannelId: document.getElementById("discordLogChannelId"),
+  discordDashboardOnlinePlayersMessageId: document.getElementById("discordDashboardOnlinePlayersMessageId"),
+  discordDashboardPalworldConfigMessageId: document.getElementById("discordDashboardPalworldConfigMessageId"),
+  discordDashboardBotConfigMessageId: document.getElementById("discordDashboardBotConfigMessageId"),
+
+  discordCommandAliasKick: document.getElementById("discordCommandAliasKick"),
+  discordCommandAliasBan: document.getElementById("discordCommandAliasBan"),
+  discordCommandAliasBroadcast: document.getElementById("discordCommandAliasBroadcast"),
+  discordCommandAliasShutdown: document.getElementById("discordCommandAliasShutdown"),
+  discordCommandAliasDoExit: document.getElementById("discordCommandAliasDoExit"),
+  discordCommandAliasSave: document.getElementById("discordCommandAliasSave"),
+  discordCommandAliasStartServer: document.getElementById("discordCommandAliasStartServer"),
+};
