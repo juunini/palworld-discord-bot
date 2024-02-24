723fadf970055f0809627bbd3423cea76a994761
Juunini <juuni.ni.i@gmail.com>
2024-02-24 22:49:28 +0900

    Web server i18n (#11)
    * i18n, web: add web config i18n
    
    * i18n, web: add buttons i18n
    
    * i18n, web: apply languages section
    
    * i18n, web: apply discord bot config i18n
    
    * i18n, web: apply palworld config i18n
    
    * i18n, web: apply discord channel config i18n
    
    * i18n, web: apply command aliases i18n

--- src/i18n/i18n.go
+++ src/i18n/i18n.go
@@ -1,8 +1,8 @@
 package i18n
 
 var Languages = []map[string]string{
-	{"CODE": "ko", "NAME": "한국어"},
-	{"CODE": "en", "NAME": "English"},
+	{"code": "ko", "name": "한국어"},
+	{"code": "en", "name": "English"},
 }
 
 func SetLanguage(language string) {
@@ -15,17 +15,79 @@ func SetLanguage(language string) {
 	}
 }
 
-var BotRunningStart string
-var FailedToConnectRconServer string
-var FailedToSaveCommand string
-var FailedToDoExitCommand string
-var FailedToShutdownCommand string
-var FailedToBroadcastCommand string
-var FailedToKickCommand string
-var FailedToBanCommand string
-var FailedToShowPlayerCommand string
-var FailedToStartServerCommand string
-var SuccessToStartServerCommand string
-var WrongParameters string
-var Help func(commandPrefix string, isAdmin bool) string
-var UnknownCommand string
+var (
+	BotRunningStart             string
+	FailedToConnectRconServer   string
+	FailedToSaveCommand         string
+	FailedToDoExitCommand       string
+	FailedToShutdownCommand     string
+	FailedToBroadcastCommand    string
+	FailedToKickCommand         string
+	FailedToBanCommand          string
+	FailedToShowPlayerCommand   string
+	FailedToStartServerCommand  string
+	SuccessToStartServerCommand string
+	WrongParameters             string
+	UnknownCommand              string
+	Help                        func(commandPrefix string, isAdmin bool) string
+)
+
+var (
+	WebConfig                    string
+	EnableWebServer              string
+	EnableWebServerDisablePrompt string
+	WebServerPort                string
+	WebServerPortTooltip         string
+
+	Language       string
+	SelectLanguage string
+
+	DiscordBotConfig                   string
+	EnableDiscordBot                   string
+	DiscordBotToken                    string
+	DiscordBotTokenTooltip             string
+	DiscordAdminUsernames              string
+	DiscordAdminUsernamesTooltip       string
+	DiscordCommandCaseSensitive        string
+	DiscordCommandCaseSensitiveTooltip string
+	DiscordCommandPrefix               string
+	DiscordCommandPrefixTooltip        string
+
+	PalworldConfig                    string
+	EnablePalworldRcon                string
+	PalworldRconHost                  string
+	PalworldRconHostTooltip           string
+	PalworldRconPort                  string
+	PalworldRconPortTooltip           string
+	PalworldAdminPassword             string
+	PalworldAdminPasswordTooltip      string
+	PalworldServerFilePath            string
+	PalworldServerFilePathTooltip     string
+	PalworldServerExecuteFlags        string
+	PalworldServerExecuteFlagsTooltip string
+
+	DiscordChannelConfig                           string
+	DiscordDashboardChannelID                      string
+	DiscordDashboardChannelIDTooltip               string
+	DiscordLogChannelID                            string
+	DiscordLogChannelIDTooltip                     string
+	DiscordDashboardOnlinePlayersMessageID         string
+	DiscordDashboardOnlinePlayersMessageIDTooltip  string
+	DiscordDashboardPalworldConfigMessageID        string
+	DiscordDashboardPalworldConfigMessageIDTooltip string
+	DiscordDashboardBotConfigMessageID             string
+	DiscordDashboardBotConfigMessageIDTooltip      string
+
+	DiscordCommandAliases                 string
+	DiscordCommandAliasKickTooltip        string
+	DiscordCommandAliasBanTooltip         string
+	DiscordCommandAliasBroadcastTooltip   string
+	DiscordCommandAliasShutdownTooltip    string
+	DiscordCommandAliasDoExitTooltip      string
+	DiscordCommandAliasSaveTooltip        string
+	DiscordCommandAliasStartServerTooltip string
+
+	Close   string
+	Confirm string
+	Save    string
+)
