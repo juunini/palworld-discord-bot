47e331576ac63a84fe2ef59351256034f3c5a778
juunini <juuni.ni.i@gmail.com>
2024-02-24 18:32:30 +0900

    web: add palworld config

--- src/web/listen.go
+++ src/web/listen.go
@@ -24,12 +24,20 @@ func Listen(port int) error {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("public/index", fiber.Map{
-			"LANGUAGE":                    config.LANGUAGE,
 			"DISCORD_BOT_ENABLED":         config.DISCORD_BOT_ENABLED,
 			"DISCORD_BOT_TOKEN":           config.DISCORD_BOT_TOKEN,
 			"DISCORD_ADMIN_USERNAMES":     strings.Join(config.DISCORD_ADMIN_USERNAMES, ", "),
 			"DISCORD_COMMAND_IGNORE_CASE": config.DISCORD_COMMAND_IGNORE_CASE,
 			"DISCORD_COMMAND_PREFIX":      config.DISCORD_COMMAND_PREFIX,
+
+			"PALWORLD_RCON_ENABLED":         config.PALWORLD_RCON_ENABLED,
+			"PALWORLD_RCON_HOST":            config.PALWORLD_RCON_HOST,
+			"PALWORLD_RCON_PORT":            config.PALWORLD_RCON_PORT,
+			"PALWORLD_ADMIN_PASSWORD":       config.PALWORLD_ADMIN_PASSWORD,
+			"PALWORLD_SERVER_FILE_PATH":     config.PALWORLD_SERVER_FILE_PATH,
+			"PALWORLD_SERVER_EXECUTE_FLAGS": strings.Join(config.PALWORLD_SERVER_EXECUTE_FLAGS, " "),
+
+			"LANGUAGE": config.LANGUAGE,
 		})
 	})
 
